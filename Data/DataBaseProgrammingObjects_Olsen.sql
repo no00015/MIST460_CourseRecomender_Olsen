090b44fe1b4a358e94129cc8ad4f2a10d130a73e
no00015 <no00015@mix.wvu.edu>
2026-02-23 18:44:17 -0500

    class

--- Data/DataBaseProgrammingObjects_Olsen.sql
+++ Data/DataBaseProgrammingObjects_Olsen.sql
@@ -2,5 +2,6 @@
 select *
 from Section
 where SemesterYear = 2026 and SectionSemester = Spring;
---2
-doing this now
+--2 What are the prerequisites for a specific, course (optional entry)?
+select *
+from CoursePrerequisite;
