561094e4948bc4514e105d8b286715b8c2595636
no00015 <no00015@mix.wvu.edu>
2026-02-23 18:58:08 -0500

    updated class

--- Data/DataBaseProgrammingObjects_Olsen.sql
+++ Data/DataBaseProgrammingObjects_Olsen.sql
@@ -1,7 +1,10 @@
 --1
 select *
-from Section
+from Section as S
+inner join Course as C on S.CourseID = C.CourseID
 where SemesterYear = 2026 and SectionSemester = Spring;
 --2 What are the prerequisites for a specific, course (optional entry)?
 select *
-from CoursePrerequisite;
+from CoursePrerequisite as CP
+inner join Course as C on CP.PrerequisiteID = C.CourseID
+where CP.CourseID = @CourseID;
