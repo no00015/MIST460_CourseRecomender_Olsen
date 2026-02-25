15c463174cbd35a8048b5f4420dfd4a6a9d12b33
no00015 <no00015@mix.wvu.edu>
2026-02-25 13:50:48 -0500

    Updated HW for 460

--- Data/DataBaseProgrammingObjects_Olsen.sql
+++ Data/DataBaseProgrammingObjects_Olsen.sql
@@ -8,3 +8,10 @@ select *
 from CoursePrerequisite as CP
 inner join Course as C on CP.PrerequisiteID = C.CourseID
 where CP.CourseID = @CourseID;
+
+--3 Has a specific student completed the prerequisites for a specific course?
+select *
+from RegistrationSection as R
+inner join Section as S on R.SectionID = S.SectionID
+inner join Course as C on S.CourseID = C.CourseID
+where R.StudentID = @StudentID and C.CourseID = @CourseID and R.EnrollmentStatus = 'Completed';
