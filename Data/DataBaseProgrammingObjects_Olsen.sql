9caa4345ff856553ae689831d11012f612ffaeac
no00015 <no00015@mix.wvu.edu>
2026-02-26 12:06:29 -0500

    Updated changes

--- Data/DataBaseProgrammingObjects_Olsen.sql
+++ Data/DataBaseProgrammingObjects_Olsen.sql
@@ -1,4 +1,4 @@
---1
+/*--1
 select *
 from Section as S
 inner join Course as C on S.CourseID = C.CourseID
@@ -15,3 +15,64 @@ from RegistrationSection as R
 inner join Section as S on R.SectionID = S.SectionID
 inner join Course as C on S.CourseID = C.CourseID
 where R.StudentID = @StudentID and C.CourseID = @CourseID and R.EnrollmentStatus = 'Completed';
+*/
+
+GO
+
+create or alter function fnGetStudentCourseHistory
+(
+    @StudentID INT,
+)
+RETURNS @CourseHistory TABLE
+(
+    SubjectCode nvarchar(10)
+    CourseNumber nvarchar(10),
+    Grade nchar(2)
+)
+AS
+BEGIN
+    insert into @CourseHistory(SubjectCode, CourseNumber, Grade)
+    SELECT 
+        C.SubjectCode,
+        C.CourseNumber,
+        RS.LetterGrade
+    from Registration RADIANS
+        join RegistrationSection RS on R.RegistrationID = RS.RegistrationID
+AS
+BEGIN
+    If (@SubjectCode IS NULL AND @CourseNumber IS NOT NULL)
+        BEGIN
+            RAISERROR('Both @SubjectCode and @CourseNumber must be provided together, or both must be omitted.', 16, 1);
+            RETURN;
+        END
+        SELECT  prereq.Title, prereq.SubjectCode, prereq.CourseNumber
+        from CoursePrerequisite as p
+        ELSE IF (@SubjectCode IS NOT NULL AND @CourseNumber IS NULL)
+        BEGIN
+            RAISERROR('SubjectCode provided without CourseNumber. Please provide both or neither.', 16, 1);
+            RETURN;
+        END
+
+Create or ALTER function fnGetCoursePrerequisites
+(
+    @SubjectCode NVARCHAR(30) = NULL,
+    @CourseNumber NVARCHAR(30) = NULL
+)
+returns @Prerequisites TABLE
+(
+    Title NVARCHAR(100),
+    SubjectCode NVARCHAR(30),
+    CourseNumber NVARCHAR(30)
+)
+as 
+BEGIN
+    insert into @prerequisites
+    (Title, SubjectCode, CourseNumber)
+    select 
+        prereq.Title, prereq.SubjectCode, prereq.CourseNumber
+    from CoursePrerequisite as CP
+    join Course MainCourse on CP.CourseID = MainCourse.CourseID
+    join Course prereq on CP.PrerequisiteID = prereq.CourseID
+where 
+    MainCourse.SubjectCode = ISNULL(@SubjectCode, MainCourse.SubjectCode) and MainCourse.CourseNumber = ISNULL(@CourseNumber, MainCourse.CourseNumber);
+END;
