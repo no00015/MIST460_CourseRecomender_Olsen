60ea2f35750566cc7b45d9192b7207ec175e84c7
no00015 <no00015@mix.wvu.edu>
2026-03-23 21:08:33 -0400

    commit

--- Data/DataBaseProgrammingObjects_Olsen.sql
+++ Data/DataBaseProgrammingObjects_Olsen.sql
@@ -30,7 +30,51 @@ where R.StudentID = @StudentID and C.CourseID = @CourseID and R.EnrollmentStatus
 */
 
 
-create or alter procedure GetCourseSectionsForSpecifiedCourse
+IF OBJECT_ID('procGetCourseSectionsForSpecifiedCourse') IS Not NULL
+drop procedure GetCourseSectionsForSpecifiedCourse;
+
+IF OBJECT_ID('fnGetSemesterFromMonth') IS Not NULL
+Drop function fnGetSemesterFromMonth;
+
+IF OBJECT_ID('fnGetCoursePrerequisites') IS Not NULL
+Drop function fnGetCoursePrerequisites;
+
+IF OBJECT_ID('fnGetStudentCourseHistory') IS Not NULL
+Drop function fnGetStudentCourseHistory;
+
+If OBJECT_ID('fnGetGradePointsFromLetterGrade') IS Not NULL
+Drop function fnGetGradePointsFromLetterGrade;
+
+If OBJECT_ID('trgDecreaseSectionSeats') IS Not NULL
+Drop trigger trgDecreaseSectionSeats;
+
+If OBJECT_ID('procValidateUser') IS Not NULL
+Drop procedure procValidateUser;
+
+IF OBJECT_ID('procHasStudentMetPrerequisitesForCourse') IS Not NULL
+Drop procedure procHasStudentMetPrerequisitesForCourse;
+
+
+GO
+create or alter function dbo.fnGetSemesterFromMonth()
+returns nvarchar(20)
+AS BEGIN
+    DECLARE @CurrentMonth INT = MONTH(GETDATE());
+    DECLARE @Semester NVARCHAR(20);
+
+    IF @CurrentMonth IN (1, 2, 3, 4, 5)
+        SET @Semester = 'Spring';
+    ELSE IF @CurrentMonth IN (6, 7)
+        SET @Semester = 'Summer';
+    ELSE
+        SET @Semester = 'Fall';
+
+    RETURN @Semester;
+END;
+
+GO
+
+create or alter function functionGetCourseSectionsForSpecifiedCourse
 (
     @SubjectCode nvarchar(15) = null,
     @CourseNumber nvarchar(15) = null
@@ -46,7 +90,56 @@ and S.SectionYear = Year(GetDate())
 and C.SubjectCode = ISNULL(@SubjectCode, C.SubjectCode)
 and C.CourseNumber = ISNULL(@CourseNumber, C.CourseNumber);
 
+END;
+
 GO
+
+create or alter procedure procGetCoursePrerequisites
+    @SubjectCode NVARCHAR(30) = NULL,
+    @CourseNumber NVARCHAR(30) = NULL
+AS
+BEGIN
+    If (@SubjectCode is Null and @CourseNumber is not Null)
+        BEGIN
+            RAISERROR('Both @SubjectCode and @CourseNumber must be provided together, or both must be omitted.');
+            RETURN;
+        END;
+    SELECT 
+        prereq.Title AS 'PrerequisiteTitle', prereq.SubjectCode AS 'PrerequisiteSubjectCode', prereq.CourseNumber AS 'PrerequisiteCourseNumber', MainCourse.Title AS 'MainCourseTitle', MainCourse.SubjectCode AS 'MainCourseSubjectCode', MainCourse.CourseNumber AS 'MainCourseNumber', CP.MinGradeRequired AS 'MinimumGradeRequired'
+    FROM CoursePrerequisite AS p
+    JOIN Course MainCourse ON p.CourseID = MainCourse.CourseID
+    JOIN Course prereq ON p.PrerequisiteID = prereq.CourseID
+    WHERE 
+        MainCourse.SubjectCode = ISNULL(@SubjectCode, MainCourse.SubjectCode) 
+        AND MainCourse.CourseNumber = ISNULL(@CourseNumber, MainCourse.CourseNumber);
+END;
+
+GO
+create or alter function fnGetCoursePrerequisites
+(
+    @SubjectCode NVARCHAR(30) = NULL,
+    @CourseNumber NVARCHAR(30) =
+)
+returns @Prerequisites TABLE
+(
+    Title NVARCHAR(100),
+    SubjectCode NVARCHAR(30),
+    CourseNumber NVARCHAR(30),
+    MinGradeRequired NVARCHAR(2)
+)
+as 
+BEGIN
+    insert into @prerequisites
+    (Title, SubjectCode, CourseNumber, MinGradeRequired)
+    select 
+        prereq.Title, prereq.SubjectCode, prereq.CourseNumber, CP.MinGradeRequired
+    from CoursePrerequisite as CP
+    join Course MainCourse on CP.CourseID = MainCourse.CourseID
+    join Course prereq on CP.PrerequisiteID = prereq.CourseID
+where 
+    MainCourse.SubjectCode = ISNULL(@SubjectCode, MainCourse.SubjectCode) and MainCourse.CourseNumber = @CourseNumber;
+    return;
+END;
 
 create or alter function fnGetStudentCourseHistory
 (
@@ -65,10 +158,59 @@ BEGIN
         C.SubjectCode,
         C.CourseNumber,
         RS.LetterGrade
-    from Registration RADIANS
+    from Registration R
         join RegistrationSection RS on R.RegistrationID = RS.RegistrationID
+        join Section S on RS.SectionID = S.SectionID
+        join Course C on S.CourseID = C.CourseID
+        where R.StudentID = @StudentID;
+        return;
+END;
+
+
+create or alter function fnGradePointsFromLetterGrade
+(
+    @LetterGrade NVARCHAR(2)
+)
+RETURNS INT
 AS
 BEGIN
+
+declare @GradePoints int;
+    set @GradePoints = case @LetterGrade
+        when 'A' then 4
+        when 'B' then 3
+        when 'C' then 2
+        when 'D' then 1
+        else 0
+    end;
+    return @GradePoints;
+    end;
+
+GO
+create or alter trigger trgDecreaseSectionSeats
+on RegistrationSection 
+after INSERT
+AS
+BEGIN
+    update Section
+    set RemainingOpenings = RemainingOpenings - 1
+    from Section as S
+    join inserted as I on S.SectionID = I.SectionID;
+    end;
+
+GO
+create procedure procEnrollStudentInSection
+(
+    @RegistrationID INT,
+    @SectionID INT
+)
+as
+BEGIN
+insert into RegistrationSection (RegistrationID, SectionID)
+values (@RegistrationID, @SectionID);
+end;
+
+select
     If (@SubjectCode IS NULL AND @CourseNumber IS NOT NULL)
         BEGIN
             RAISERROR('Both @SubjectCode and @CourseNumber must be provided together, or both must be omitted.', 16, 1);
