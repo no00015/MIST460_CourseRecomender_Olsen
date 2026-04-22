491898d0a7656158e71cd03e47834aee68e33804
no00015 <no00015@mix.wvu.edu>
2026-04-22 17:09:06 -0400

    tes

--- API/get_db_connection.py
+++ API/get_db_connection.py
@@ -1,4 +1,5 @@
-import pyodbc
+#import pyodbc
+import pymssql
 import os
 from dotenv import load_dotenv
 
@@ -11,8 +12,12 @@ def get_db_connection():
     database = os.getenv('DB_NAME')
     user = os.getenv('DB_USER')
     password = os.getenv('DB_PASSWORD')
-    driver = os.getenv('DB_DRIVER')
+    #driver = os.getenv('DB_DRIVER')
 
-    connection_string = f"DRIVER={driver};SERVER={server}; DATABASE={database};UID={user};PWD={password};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;"
+    #connection_string = f"DRIVER={driver};SERVER={server}; DATABASE={database};UID={user};PWD={password};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;"
 
-    return pyodbc.connect(server=server, user=user, password=password, database=database, port =1433, tds_version='7.4')
+    #connection_string = f"DRIVER={driver};SERVER={server}; DATABASE={database};UID={user};PWD={password};"
+    #connection_string += f"Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;"
+
+    #return pyodbc.connect(connection_string)
+    return pymssql.connect(server=server, user=user, password=password, database=database, port =1433, tds_version='7.4')
